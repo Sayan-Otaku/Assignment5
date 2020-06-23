1c42b036aed24c231ac88920d8f7a46917f50e40
Sayan-Otaku <67100606+Sayan-Otaku@users.noreply.github.com>
2020-06-23 14:24:31 +0530

    Add files via upload

---
+++ area_of_segment.py
@@ -0,0 +1,21 @@
+import math
+  
+pi = 3.14 
+def area_of_segment(radius, angle): 
+    area_of_sector = pi * 
+                     (radius * radius) 
+                     * (angle / 360) 
+   
+    area_of_triangle = 1 / 2 * 
+                       (radius * radius) *
+                       math.sin((angle * pi) / 180) 
+  
+    return area_of_sector - area_of_triangle; 
+   
+radius = int(input("Enter the radius of cu=ircle:-"))
+angle = 90.0
+print("Area of minor segment =", 
+       area_of_segment(radius, angle)) 
+print("Area of major segment =", 
+      area_of_segment(radius, (360 - angle))) 
+        
---
+++ math-func.py
@@ -0,0 +1,23 @@
+import math
+print("The value of sin(60) = {}\n".format(math.sin(math.pi/3)))
+
+print("The value of cos(pi) = {}\n".format(math.cos(math.pi)))
+
+print("The value of tan(90) = {}\n".format(math.tan(math.pi/2)))
+
+print("The value of angle of sin(0.8660254037844386) = {}\n".format(math.asin(0.866025403784438)))
+
+print("The value of 5^8 = {}\n".format(pow(5,8)))
+
+print("The value of square root of 400= {}\n".format(math.sqrt(400)))
+
+print("The value of 5^e = {}\n".format(pow(5,2.71828)))
+
+print("The value of log(1024) at base 2 = {}\n".format(math.log2(1024)))
+
+print("The value of log(1024) at base 10 = {}\n".format(math.log10(1024)))
+
+print("The floor value of 23.56 = {}\n".format(math.floor(23.56)))
+
+print("The ceil value of 23.56 = {}\n".format(math.ceil(23.56)))
+
